1b40a199afef6102f9a0aae6f4ad986fa59e5202
TheArkNoah05 <55030669+Noach05@users.noreply.github.com>
2020-11-19 16:53:53 +0100

    Update MondasianCyberman.java

--- src/main/java/TheArkNoah05/DoctorWhoMobs/entities/MondasianCyberman.java
+++ src/main/java/TheArkNoah05/DoctorWhoMobs/entities/MondasianCyberman.java
@@ -1,32 +1,40 @@
 package TheArkNoah05.DoctorWhoMobs.entities;
 
-import TheArkNoah05.DoctorWhoMobs.init.DWMEntities;
-import net.minecraft.entity.CreatureEntity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.goal.LookRandomlyGoal;
-import net.minecraft.entity.ai.goal.RandomWalkingGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
-import net.minecraft.entity.monster.SkeletonEntity;
+import net.minecraft.entity.*;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
 
-public class MondasianCyberman extends SkeletonEntity {
+import javax.annotation.Nullable;
 
-    @SuppressWarnings("unchecked")
+public class MondasianCyberman extends CreatureEntity {
+
     public MondasianCyberman(EntityType<? extends CreatureEntity> type, World worldIn) {
-        super((EntityType<? extends SkeletonEntity>) DWMEntities.mondasian_cyberman, worldIn);
+        super(type, worldIn);
     }
 
     protected void registerGoals() {
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(2, new RandomWalkingGoal(this, 1.2d));
-        this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
+        this.goalSelector.addGoal(6, new LookRandomlyGoal(this));
+        this.goalSelector.addGoal(6, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
     }
 
     @Override
     protected void registerAttributes() {
         super.registerAttributes();
         this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20d);
-        this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23F);
+        this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
+    }
+
+    @Nullable
+    public ILivingEntityData onInitialSpawn(IWorld worldIn, DifficultyInstance difficultyIn, SpawnReason reason, @Nullable ILivingEntityData spawnDataIn, @Nullable CompoundNBT dataTag) {
+        spawnDataIn = super.onInitialSpawn(worldIn, difficultyIn, reason, spawnDataIn, dataTag);
+        return spawnDataIn;
     }
 }
